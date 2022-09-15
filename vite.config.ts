78772c78e60f0d3aa71bf7ae82dd09a8506e88dd
Jakkzero <isleyfoster@gmail.com>
2022-09-15 21:42:44 +0100

    Fix reseller not being built

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,11 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+      rollupOptions: {
+        input: {
+          index: './index.html',
+          reseller: './reseller/index.html'
+        }
+      }
   },
 });
